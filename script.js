b4324cabe4492ee1f1bb0a211cb6f59c1c57c7e4
andreazampieri <andreazampieri95@gmail.com>
2016-11-09 22:42:04 +0100

    worng branch: developing gradually functionalities

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ function toggle_display() {
 
 var stuff = {
 	names:["Chair","Candies","Pens"], 
-	quantities:[1,5,2]
+	quantities:[1,5,2],
+	addItem : function(name,quantity){
+		if((i=names.indexof(name))!=-1){
+			quantities[i] += quantity;
+		}else{
+			names.push(name);
+			quantities.push(quantity);
+		}
+	}
 };
 
 function displayTable(){
 	var table = document.getElementById("table");
-	
+	var length = table.rows.length;
+	for(var i=0;i<length;i++){
+		table.deleteRow(0);
+	}
 	table.insertRow(0);
 	for(var i=0;i<stuff.quantities.length;i++){
 		newcell=table.rows[0].insertCell(i);
@@ -21,4 +32,8 @@ function displayTable(){
 		newcell=table.rows[0].insertCell(i);
 		newcell.innerHTML=stuff.names[i];
 	}
+}
+
+function addItems(){
+
 }
